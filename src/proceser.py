94f866a8d074c55d9c9669f301f5a11e8e0f1689
alex boyle <aboyle75@gmail.com>
2023-09-25 00:17:24 -0500

    ?

--- src/proceser.py
+++ src/proceser.py
@@ -54,6 +54,37 @@ def find_conturs(ogImage):
     #out = cv2.threshold(out, 250, 255, cv2.THRESH_BINARY)[1]
 
     cv2.imwrite("./output/final.jpg", out)
+def find_conturs_alt(ogImage):
+    image = ogImage.copy()
+    original = image.copy()
+    #Find the relevent parts of the image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
+    dilated = cv2.dilate(blurred, rectKernel)
+    edged = cv2.Canny(dilated, 100, 200, apertureSize=3)
+    contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    image_with_contours = cv2.drawContours(image.copy(), contours, -1, (0,255,0), 3)
+    cv2.imwrite("./output/image_with_contours.jpg", image_with_contours)
+    largest_contours = sorted(contours, key = cv2.contourArea, reverse = True)[:1]
+    receipt_contour = largest_contours[0]#get_receipt_contour(largest_contours)
+    image_with_receipt_contour = cv2.drawContours(image.copy(), [receipt_contour], -1, (0, 255, 0), 2)
+    cv2.imwrite("./output/image_with_receipt_contour.jpg", image_with_receipt_contour)
+    mask = np.zeros_like(image)
+    mask = cv2.drawContours(image=mask, contours=[receipt_contour], contourIdx=-1, color=(255, 255, 255), thickness=-1, lineType=cv2.LINE_AA)
+    out = np.zeros_like(mask)
+    out[mask == 255] = image[mask == 255]
+    cv2.imwrite("./output/mask.jpg", mask)
+    cv2.imwrite("./output/cut.jpg", out)
+    gray = cv2.cvtColor(mask,cv2.COLOR_BGR2GRAY)
+    contours = cv2.findContours(gray,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
+    cnt = contours[0]
+    x,y,w,h = cv2.boundingRect(cnt)
+    out = out[y:y+h,x:x+w]
+    cv2.imwrite('./output/crop.jpg',out)
+    final = cv2.cvtColor(out, cv2.COLOR_BGR2GRAY)
+    #out = cv2.resize(out, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+    final = cv2.adaptiveThreshold(final, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 20)
 if __name__ == "__main__":
     print(sys.argv[1])
     img = cv2.imread(sys.argv[1])
